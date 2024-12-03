3a8513e2da9386cee76f3c7441bfb06de43afc7d
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 <matthew.d.lundberg@gmail.com>
2024-12-03 10:25:28 -0700

    Updating the Menu
    Updating the menu to have submenus. This allows for more items and better user experience.

--- menu.py
+++ menu.py
@@ -1,21 +1,54 @@
 # Menu data
-MENU_ITEMS = [
-    {"Number": 1, "Item Name": "Hamburger", "Price": 3.99, "Type": "Meal"},
-    {"Number": 2, "Item Name": "Cheeseburger", "Price": 4.99, "Type": "Meal"}, 
-    {"Number": 3, "Item Name": "Chicken Nuggets", "Price": 2.99, "Type": "Meal"},
-    {"Number": 4, "Item Name": "Chicken Tenders", "Price": 3.99, "Type": "Meal"},
-    {"Number": 5, "Item Name": "Chicken Fingers", "Price": 3.99, "Type": "Meal"},
-    {"Number": 6, "Item Name": "Chicken Sandwich", "Price": 4.99, "Type": "Meal"},
-    {"Number": 7, "Item Name": "Chicken Wrap", "Price": 4.99, "Type": "Meal"},
-    {"Number": 8, "Item Name": "Chips", "Price": 1.99, "Type": "Snack"},
-    {"Number": 9, "Item Name": "Popcorn", "Price": 0.99, "Type": "Snack"},
-    {"Number": 10, "Item Name": "Candy", "Price": 0.50, "Type": "Snack"},
-    {"Number": 11, "Item Name": "Soda", "Price": 1.99, "Type": "Drink"},
-    {"Number": 12, "Item Name": "Water", "Price": 0.99, "Type": "Drink"},
-    {"Number": 13, "Item Name": "Coffee", "Price": 0.50, "Type": "Drink"},
-    {"Number": 14, "Item Name": "Ice Cream", "Price": 1.99, "Type": "Dessert"},
-    {"Number": 15, "Item Name": "Cake", "Price": 0.99, "Type": "Dessert"},
-    {"Number": 16, "Item Name": "Pie", "Price": 0.50, "Type": "Dessert"}
+MENU_ITEMS = [{
+    "type": "Meal", 
+    "items": [
+        {"Number": 1, "Item Name": "Hamburger", "Price": 3.99},
+        {"Number": 2, "Item Name": "Cheeseburger", "Price": 4.99}, 
+        {"Number": 3, "Item Name": "Chicken Nuggets", "Price": 2.99},
+        {"Number": 4, "Item Name": "Chicken Tenders", "Price": 3.99},
+        {"Number": 5, "Item Name": "Chicken Fingers", "Price": 3.99},
+        {"Number": 6, "Item Name": "Chicken Sandwich", "Price": 4.99},
+        {"Number": 7, "Item Name": "Chicken Wrap", "Price": 4.99},
+    ]
+},  
+    {"type": "Snack", 
+    "items": [
+        {"Number": 1, "Item Name": "Chips", "Price": 1.99},
+        {"Number": 2, "Item Name": "Popcorn", "Price": 0.99},
+        {"Number": 3, "Item Name": "Candy", "Price": 0.50},
+        {"Number": 4, "Item Name": "Pretzels", "Price": 1.50},
+        {"Number": 5, "Item Name": "Trail Mix", "Price": 2.99},
+        {"Number": 6, "Item Name": "Crackers", "Price": 1.25},
+        {"Number": 7, "Item Name": "Nuts", "Price": 2.50},
+        {"Number": 8, "Item Name": "Fruit Snacks", "Price": 0.99},
+    ]
+},
+    {"type": "Drink", 
+    "items": [
+        {"Number": 1, "Item Name": "Soda", "Price": 1.99},
+        {"Number": 2, "Item Name": "Water", "Price": 0.99}, 
+        {"Number": 3, "Item Name": "Coffee", "Price": 0.50},
+        {"Number": 4, "Item Name": "Tea", "Price": 0.99},
+        {"Number": 5, "Item Name": "Lemonade", "Price": 1.50},
+        {"Number": 6, "Item Name": "Juice", "Price": 1.99},
+        {"Number": 7, "Item Name": "Smoothie", "Price": 2.99},
+        {"Number": 8, "Item Name": "Hot Chocolate", "Price": 1.50},
+        {"Number": 9, "Item Name": "Iced Tea", "Price": 1.50},
+        {"Number": 10, "Item Name": "Energy Drink", "Price": 2.99},
+    ]
+},
+    {"type": "Dessert", 
+    "items": [
+        {"Number": 1, "Item Name": "Ice Cream", "Price": 1.99},
+        {"Number": 2, "Item Name": "Cake", "Price": 0.99}, 
+        {"Number": 3, "Item Name": "Pie", "Price": 0.50},
+        {"Number": 4, "Item Name": "Brownie", "Price": 1.50},
+        {"Number": 5, "Item Name": "Cookies", "Price": 0.99},
+        {"Number": 6, "Item Name": "Milkshake", "Price": 2.99},
+        {"Number": 7, "Item Name": "Sundae", "Price": 3.99},
+        {"Number": 8, "Item Name": "Cheesecake", "Price": 2.99}
+    ]
+}
 ]
 
 class OrderSystem:
@@ -26,15 +59,27 @@ class OrderSystem:
         self.quantity_spacing = 5
 
     def _find_name_spacing(self):
-        return max(len(item["Item Name"]) for item in MENU_ITEMS)
+        name_spacing = 0
+        for item in MENU_ITEMS:
+            for sub_item in item["items"]:
+                if len(sub_item["Item Name"]) > name_spacing:
+                    name_spacing = len(sub_item["Item Name"])
+        return name_spacing
 
     def _find_price_spacing(self):
-        return max(len(str(item["Price"])) for item in MENU_ITEMS)
+        price_spacing = 0
+        for item in MENU_ITEMS:
+            for sub_item in item["items"]:
+                if len(str(sub_item["Price"])) > price_spacing:
+                    price_spacing = len(str(sub_item["Price"]))
+        return price_spacing
 
     def print_menu(self):
         print("Menu")
+        index = 1
         for item in MENU_ITEMS:
-            print(f"{item['Type']:<8} | {item['Number']:<2} | {item['Item Name']:<{self.name_spacing}} | ${item['Price']:<{self.price_spacing}.2f}")
+            print(f"{index}: {item['type']:<8}")
+            index += 1
 
     def print_order(self):
         print("You have ordered:")
@@ -47,14 +92,20 @@ class OrderSystem:
 
     def get_menu_selection(self):
         while True:
+            selection = input("Please select an group from the menu: ")
+            if selection.isdigit() and int(selection) <= len(MENU_ITEMS):
+                return MENU_ITEMS[int(selection) - 1]
+            print("Please enter a number for an item on the menu")
+
+    def get_sub_menu_selection(self, menu_item):
+        for item in menu_item["items"]:
+            print(f"{item['Number']:<2} | {item['Item Name']:<{self.name_spacing}} | ${item['Price']:<{self.price_spacing}.2f}")
+        while True:
             selection = input("Please select an item from the menu: ")
-            if selection.isdigit():
+            if selection.isdigit() and int(selection) <= len(menu_item["items"]):
                 selection = int(selection)
-                if 1 <= selection <= len(MENU_ITEMS):
-                    return MENU_ITEMS[selection - 1]
-                print("Please enter a number for an item on the menu")
-            else:
-                print("Please enter a valid number")
+                return menu_item["items"][selection - 1]
+            print("Please enter a valid number")
 
     def get_quantity(self):
         while True:
@@ -69,7 +120,7 @@ class OrderSystem:
         self.print_menu()
         
         while True:
-            selected_item = self.get_menu_selection()
+            selected_item = self.get_sub_menu_selection(self.get_menu_selection())
             quantity = self.get_quantity()
             
             self.ordered_items.append({
